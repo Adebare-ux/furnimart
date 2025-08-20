f7edb139b0c501210936bcf73d7775357ee48bc5
Adebare <adeagbomaliqadebare@gmail.com>
2025-08-20 18:30:54 +0100

    Add button component
    Update homepage to use button component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,5 @@
+import Button from "../../components/Button";
+
 const Home = () => {
   return (
     <>
@@ -7,17 +9,9 @@ const Home = () => {
       </div>
 
       <div className="flex gap-4 m-8">
-        <button className="bg-[#4CAF50] text-white px-5 py-2.5 text-center no-underline inline-block text-base m-1 cursor-pointer rounded-md">
-          Sofa
-        </button>
-
-        <button className="bg-[#4CAF50] text-white px-5 py-2.5 text-center no-underline inline-block text-base m-1 cursor-pointer rounded-md">
-          Chairs
-        </button>
-
-        <button className="bg-[#4CAF50] text-white px-5 py-2.5 text-center no-underline inline-block text-base m-1 cursor-pointer rounded-md">
-          Table
-        </button>
+        <Button className="btn">Sofa</Button>
+        <Button className="btn">Chairs</Button>
+        <Button className="btn">Tables</Button>
       </div>
 
       <div className="grid [grid-template-columns:repeat(auto-fill,minmax(200px,1fr))] gap-5 justify-center items-center m-5">
@@ -27,9 +21,7 @@ const Home = () => {
             <strong>Leather Chair</strong>
           </p>
           <p className="price">$50.00</p>
-          <button className="bg-green-800 text-white w-28 rounded-lg py-2 cursor-pointer">
-            Add to Cart
-          </button>
+          <Button className="btn !bg-green-800">Add to Cart</Button>
         </div>
 
         <div className="shadow-md flex flex-col items-center text-center w-4/5 m-2.5">
@@ -38,9 +30,7 @@ const Home = () => {
             <strong>Modern Chair</strong>
           </p>
           <p className="price">$90.00</p>
-          <button className="bg-green-800 text-white w-28 rounded-lg py-2 cursor-pointer">
-            Add to Cart
-          </button>
+          <Button className="btn !bg-green-800">Add to Cart</Button>
         </div>
 
         <div className="shadow-md flex flex-col items-center text-center w-4/5 m-2.5">
@@ -49,9 +39,7 @@ const Home = () => {
             <strong>Castle Ray Chair</strong>
           </p>
           <p className="price">$80.00</p>
-          <button className="bg-green-800 text-white w-28 rounded-lg py-2 cursor-pointer">
-            Add to Cart
-          </button>
+          <Button className="btn !bg-green-800">Add to Cart</Button>
         </div>
 
         <div className="shadow-md flex flex-col items-center text-center w-4/5 m-2.5">
@@ -60,9 +48,7 @@ const Home = () => {
             <strong>Leatherette Chair</strong>
           </p>
           <p className="price">$100.00</p>
-          <button className="bg-green-800 text-white w-28 rounded-lg py-2 cursor-pointer">
-            Add to Cart
-          </button>
+          <Button className="btn !bg-green-800">Add to Cart</Button>
         </div>
 
         <div className="shadow-md flex flex-col items-center text-center w-4/5 m-2.5">
@@ -72,9 +58,7 @@ const Home = () => {
             <strong>Two Legged Chair</strong>
           </p>
           <p className="price">$120.00</p>
-          <button className="bg-green-800 text-white w-28 rounded-lg py-2 cursor-pointer">
-            Add to Cart
-          </button>
+          <Button className="btn !bg-green-800">Add to Cart</Button>
         </div>
       </div>
     </>
